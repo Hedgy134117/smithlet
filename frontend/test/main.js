d35a2f9e4d80c16a3159bd88f9df0a0330c7e10c
Graham Smith <hedgy134117@gmail.com>
2020-10-23 13:05:27 -0400

    visual dragging, no actual dragging

--- frontend/test/main.js
+++ frontend/test/main.js
@@ -1,9 +1,22 @@
+function dragStart() {
+  this.style.opacity = '.4';
+}
+
+function dragEnd() {
+  this.style.opacity = '1';
+}
+
 function addCard() {
-    var card = `<div class="card">
+    var card = `<div class="card" draggable="true">
     <p class="bold">Name</p>
     <p>Text</p>
     `;
+    card = $(card);
+    $(card).on('dragstart', dragStart);
+    $(card).on('dragend', dragEnd);
     $(card).insertBefore(this);
 }
 
 $('button').on('click', addCard);
+$('.card').on('dragstart', dragStart);
+$('.card').on('dragend', dragEnd);
