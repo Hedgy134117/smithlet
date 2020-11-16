a8fa1276a9143d6622bf5af8ae4a38c4af3d7bfe
Hedgy134117 <hedgy134117@gmail.com>
2020-11-16 17:20:58 -0500

    restart

--- frontend/test/main.js
+++
@@ -1,9 +0,0 @@
-function addCard() {
-    var card = `<div class="card">
-    <p class="bold">Name</p>
-    <p>Text</p>
-    `;
-    $(card).insertBefore(this);
-}
-
-$('button').on('click', addCard);
